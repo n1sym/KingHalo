cc53638ed779eb503494b25b3154f6772eb3ddc5
hukurouo <owlbook248@gmail.com>
2021-12-05 02:19:36 +0900

    backendの更新

--- backend/scrape.rb
+++ backend/scrape.rb
@@ -33,17 +33,23 @@ def main
   end
   race_url_list.uniq!
   puts "finished scrape race_url_list"
+
   agent = Mechanize.new
   horse_url_list = []
+  hash = {}
   race_url_list.each do |url|
     sleep 1
     page = agent.get("https://race.netkeiba.com/race/shutuba.html?race_id=#{url}&rf=race_list")
     elements = page.xpath('//span[@class="HorseName"]/a')
+    name = page.xpath('//div[@class="RaceName"]').text.split("\n")[0]
     elements.each do |e|
-      horse_url_list << e.attribute("href").value
+      horse_url = e.attribute("href").value
+      horse_url_list << horse_url
+      hash[horse_url] = {id: url, name: name}
     end
   end
   puts "finished scrape horse_url_list"
+
   halo_list = []
   horse_url_list.each do |url|
     sleep 1
@@ -53,13 +59,23 @@ def main
     bloods.delete("")
     if bloods.include? "キングヘイロー"
       puts "hello!"
+      b_ml = page.xpath('//td[@class="b_ml"]')[0].css("a").text
+      b_ml_2 = page.xpath('//td[@class="b_ml"]')[2].css("a").text
+      b_fml = page.xpath('//td[@class="b_fml"]')[1].css("a").text
       name = page.xpath('//div[@class="horse_title"]').css('h1').text.strip.gsub(/[[:space:]]/, '')
-      hash = {
+      age = page.xpath('//div[@class="horse_title"]').css('p').text.split("　")[1]
+      color = page.xpath('//div[@class="horse_title"]').css('p').text.split("　")[2].strip.gsub(/[[:space:]]/, '')
+      halo_list << {
         horse_name: name,
-        horse_url: url
+        horse_url: url,
+        horse_age: age,
+        horse_color: color,
+        b_ml: b_ml,
+        b_ml_2: b_ml_2,
+        b_fml: b_fml,
+        race_name: hash[url][:name],
+        race_id: hash[url][:id]
       }
-      puts hash
-      halo_list << hash
     end
   end
   halo_list
