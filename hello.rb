b2092f27426d9a7c03e43b90c433182cfdec6e8b
hukurouo <owlbook248@gmail.com>
2021-12-04 12:55:29 +0900

    ciでファイル作成

--- hello.rb
+++ hello.rb
@@ -1,1 +1,9 @@
-puts "halo :D"
+require "json"
+
+arr = []
+3.times do |i|
+    arr << hash = { "name": "user#{i+1}", "age": 19+i }
+end
+File.open("test.json","w") {|file| 
+  file.puts(JSON.generate(arr))
+}
