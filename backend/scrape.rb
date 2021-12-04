9cef9dcd64f3b74b83677d32cba4df2e152ef027
hukurouo <owlbook248@gmail.com>
2021-12-04 19:26:35 +0900

    test

--- backend/scrape.rb
+++ backend/scrape.rb
@@ -32,7 +32,7 @@ def main
     race_url_list << e.attribute("href").gsub(/[^\d]/, "").to_i
   end
   race_url_list.uniq!
-  puts "finished scrape race_url_list"
+  return puts "finished scrape race_url_list"
   agent = Mechanize.new
   horse_url_list = []
   race_url_list.each do |url|
